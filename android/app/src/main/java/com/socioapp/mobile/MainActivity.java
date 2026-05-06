3a146914e4ccd37ebf968955beff92d9bb8c0a15
Suryachalam V M <suryachalam18@gmail.com>
2026-05-06 22:51:38 +0530

    Remove splash screen, bump compileSdk, add APK
    Remove SplashScreen usage and simplify MainActivity to an empty BridgeActivity. Bump compileSdkVersion from 35 to 36 in android/variables.gradle. Update AndroidManifest theme from @style/AppTheme.NoActionBarLaunch to @style/AppTheme.NoActionBar. Update .idea workspace changelist and add the built APK socio-app-definitive.apk.

--- android/app/src/main/java/com/socioapp/mobile/MainActivity.java
+++ android/app/src/main/java/com/socioapp/mobile/MainActivity.java
@@ -1,14 +1,6 @@
 package com.socioapp.mobile;
 
 import android.os.Bundle;
-import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.installSplashScreen(this);
-        super.onCreate(savedInstanceState);
-    }
-}
-
+public class MainActivity extends BridgeActivity {}

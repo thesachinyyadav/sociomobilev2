ce2a9065c58e8389fd41909c3eff4ebc37baf0f9
Suryachalam V M <suryachalam18@gmail.com>
2026-05-21 21:03:59 +0530

    chore: auto-set JAVA_HOME to Java 21 in build-apk.bat

--- build-apk.bat
+++ build-apk.bat
@@ -1,4 +1,9 @@
 @echo off
+:: Auto-set JAVA_HOME to Android Studio's bundled JRE (Java 21) to satisfy Gradle 8+ requirements
+set "JAVA_HOME=C:\Program Files\Android\Android Studio2\jbr"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+echo Using Java: %JAVA_HOME%
+
 echo [1/4] Building Next.js application...
 call npm run build
 if %ERRORLEVEL% neq 0 (
